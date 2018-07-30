8c1937c7a92c1ca453e917bc59ffba612ecb39fa
少 梁 <liangshaojiang@live.cn>
2018-07-30 14:25:10 +0800

    额222

---
+++ JwtTokenDemo/Controllers/AuthController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JwtTokenDemo.Data;
+using JwtTokenDemo.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtTokenDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private UserManager<User> userManager;
+
+        public AuthController(UserManager<User> _userManager)
+        {
+            this.userManager = _userManager;
+        }
+
+        [HttpPost]
+        [HttpGet]
+        [Route("login")]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+
+            var user = await userManager.FindByNameAsync(model.UserName);
+            if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var claims = new[] {
+                    new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti,user.Id)
+                };
+
+                var signingkey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MyJwtKsy123456!@#"));
+
+                var token = new JwtSecurityToken(
+                    //issuer: "http://www.baidu.com",
+                    //audience: "http://www.baidu.com",
+                    expires: DateTime.UtcNow.AddMinutes(1),
+                    claims: claims,
+                    signingCredentials: new Microsoft.IdentityModel.Tokens.SigningCredentials(signingkey, SecurityAlgorithms.HmacSha256)
+                    );
+                var result = new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                };
+                return Ok(result);
+            }
+
+            return Unauthorized();
+        }
+
+    }
+}
--- JwtTokenDemo/Controllers/ValuesController.cs
+++ JwtTokenDemo/Controllers/ValuesController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using JwtTokenDemo.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JwtTokenDemo.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
